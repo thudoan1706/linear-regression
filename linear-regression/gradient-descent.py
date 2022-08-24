3f90588e2591303ea9ccce651e1b136dca32f93d
thudoan1706 <62227067+thudoan1706@users.noreply.github.com>
2022-08-23 23:39:41 -0400

    Update gradient-descent.py

--- linear-regression/gradient-descent.py
+++ linear-regression/gradient-descent.py
@@ -1,8 +1,13 @@
-#The optimal values of m and b can be actually calculated with way less effort than doing a linear regression. 
-#This is just to demonstrate gradient descent
+# Gradient Descent Function
+# Here iterations, learning_rate, stopping_threshold
+# are hyperparameters that can be tuned
 
 from numpy import *
 from random import *
+
+# how many times do we want to train this model
+ITERATIONS = 100
+STOPPING_THRESHOLD = 1e-6
 
 def compute_error_for_line_given_points(b, m, points):
     totalError = 0
@@ -25,10 +30,17 @@ def step_gradient(b_current, m_current, points, learningRate):
     new_m = m_current - (learningRate * m_gradient)
     return [new_b, new_m]
 
-def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+def gradient_descent_runner(points, starting_b, starting_m, learning_rate):
     b = starting_b
     m = starting_m
-    for i in range(num_iterations):
+
+    previous_cost = None
+    for i in range(ITERATIONS):
+
+        current_cost = compute_error_for_line_given_points(b, m, points)
+        if previous_cost and abs((previous_cost) - current_cost) <= STOPPING_THRESHOLD:
+            break
+        previous_cost = current_cost
         b, m = step_gradient(b, m, array(points), learning_rate)
     return [b, m]
 
@@ -39,8 +51,10 @@ def run():
     # genfromtxt will have two loops: convert each line to string and to appropriate data type
     points = genfromtxt('data.csv', delimiter=',')
 
-    # Step 2 - define our hyperparameters: the parameters that models will use to analyze certain data (speed and operation)
-    # how fast should our model converge (optimal result, line of best fit)?
+    # Step 2 - define our hyperparameters: the parameters that models will use to analyze certain data.
+
+
+    # how fast (determine the size of step) should our model converge (optimal result, line of best fit)?
     learning_rate = 0.0001
 
     # slope formula: y = mx + b
@@ -48,15 +62,13 @@ def run():
     initial_m = 0
     # initial slope: pick random value for the intercept-b and slope-m as an initial guess that gives gradient descent something to improve upon.
 
-    # how much do we want to train this model
-    num_iterations = 100
-
     # Step 3 - train our model
     print(f"starting gradient descent at b = {initial_b}, m = {initial_m}, error = {compute_error_for_line_given_points(initial_b, initial_m, points)}")
     print("Running...")
-    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
-    print(f"After {num_iterations} iterations b = {b}, m = {m}, error = {compute_error_for_line_given_points(b, m, points)}")
+    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate)
+    print(f"After {ITERATIONS} iterations b = {b}, m = {m}, error = {compute_error_for_line_given_points(b, m, points)}")
 
 
 if __name__ == "__main__":
     run()
+
